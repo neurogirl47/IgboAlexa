9f3869af4d7d827d56242b9cc92843c5a96bb1de
Udochi Okeke <udochio@hotmail.com>
2019-05-04 11:39:28 -0400

    Add questionssml.js via upload

---
+++ questionssml.js
@@ -0,0 +1,257 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+
+  QUESTIONS_EN_US:  [
+    {
+       ' From November through December of 1929, Thousands of <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> Women led a massive revolt against British Colonial administrators, which is documented in British History by what name?': [
+        'Aba Womens Riot',
+        'The Womens War',
+        ' Uprising of the <phoneme alphabet=\'ipa\' ph=\'ɪboʊ\'> Igbo </phoneme> Women',
+        '<phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> Women Strike Back',
+        ],
+
+  },
+
+    {
+      'What role or roles have <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> women historically played in <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> traditional religious observances? ': [
+        'all of the above',
+        'Priestesses',
+        'participants in masquerade cults',
+        'traditional healers',
+      ],
+
+  },
+ 
+    {
+      'Which of the follow is NOT true about <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> female husbands?': [
+        'female husbands were women who did shunned men and did not want children',
+        'these women were usually of high social standing or past the age of menopause',
+        'the wives of these women were allowed to have boyfriends',
+        'any child of her wife would carry the name of the female husband',
+      ],
+  },
+ 
+    {
+      'How did pre-colonial <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> women contribute to the <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> economy? ': [
+        'participating in agriculture and foreign trade',
+        'developing a stock exchange',
+        'advertising her husbands products to the community',
+        'manufacturing tools',
+      ],
+  },
+    {
+      'What roles did traditional <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> women play in medicine?': [
+        'all of the above',
+        'Midwives',
+        'herbalists',
+        'mediators between humans and spirits',
+      ],
+  },
+    {
+      'What is the name of the <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> author whose self-written biography published in 1789 was instrumental to ending the transatlantic slave trade from Britain and its colonies?': [
+        'Gustavus Vassa',
+        '<phoneme alphabet=\'ipa\' ph=\'ˈtʃɪ̙nʊ̙a\'>Chinua</phoneme> <phoneme alphabet=\'ipa\' ph=\'atʃebe\'>Achebe</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'ˈnnamdɪ̙\'> Nnamdi </phoneme> <phoneme alphabet=\'ipa\' ph=\'okonkʷɔ̙\'>Okonkwo </phoneme>',
+        'Sir Edward <phoneme alphabet=\'ipa\' ph=\'madʊ̙ka\'>Maduka</phoneme>',
+      ],
+  },
+    {
+      'Which of the following is not a Nollywood actor as of October 2018?': [
+        'Chief <phoneme alphabet=\'ipa\' ph=\'obi\'>Obi</phoneme>',
+        'Genevieve <phoneme alphabet=\'ipa\' ph=\'nnadʒi\'>Nnaji</phoneme>',
+        'John <phoneme alphabet=\'ipa\' ph=\'okafɔ̙ɹ\'>Okafor</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'emeka\'>Emeka</phoneme> <phoneme alphabet=\'ipa\' ph=\'ike\'>Ike</phoneme>',
+      ],
+  },
+    {
+      'Which famous American traced his heritage to <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> culture?': [
+        'Forest Whitaker',
+        'Michael Jackson',
+        'Oprah Winfrey',
+        'Kanye West',
+      ],
+  },
+    {
+      'Which famous <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> person wrote the book Things fall apart?': [
+        '<phoneme alphabet=\'ipa\' ph=\'ˈtʃɪ̙nʊ̙a\'>Chinua</phoneme> <phoneme alphabet=\'ipa\' ph=\'atʃebe\'>Achebe</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'ˈtʃimamanda\'>Chimamanda</phoneme> <phoneme alphabet=\'ipa\' ph=\'nɡɔ̙zɪ̙\'>Ngozi</phoneme> <phoneme alphabet=\'ipa\' ph=\'aditʃie\'>Adichie</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'nnedi\'>Nnedi</phoneme> <phoneme alphabet=\'ipa\' ph=\'okoɹoafɔ̙ɹ\'>Okoroafor</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'nnamdɪ̙\'>Nnamdi</phoneme> <phoneme alphabet=\'ipa\' ph=\'azɪ̙kiwe\'>Azikiwe</phoneme>',
+      ],
+  },
+     {
+      'What <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> author wrote the best-selling book Americanah?': [
+        '<phoneme alphabet=\'ipa\' ph=\'ˈtʃimamanda\'>Chimamanda</phoneme> <phoneme alphabet=\'ipa\' ph=\'nɡɔ̙zɪ̙\'>Ngozi</phoneme> <phoneme alphabet=\'ipa\' ph=\'aditʃie\'>Adichie</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'nnedi\'>Nnedi</phoneme> <phoneme alphabet=\'ipa\' ph=\'okoɹoafɔ̙ɹ\'>Okoroafor</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'nnamdɪ̙\'>Nnamdi</phoneme> <phoneme alphabet=\'ipa\' ph=\'azɪ̙kiwe\'>Azikiwe</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'ˈtʃɪ̙nʊ̙a\'>Chinua</phoneme> <phoneme alphabet=\'ipa\' ph=\'atʃebe\'>Achebe</phoneme>',
+      ],
+  },
+    {
+      'What is the Nigerian food called pap made of?': [
+        'Corn meal',
+        'Cassava flour',
+        'Pumpkin seeds',
+        'None of the above',
+      ],
+  },
+    {
+      'What does it mean to wash beans before preparing <phoneme alphabet=\'ipa\' ph=\'mɔɪ\'>moi</phoneme> <phoneme alphabet=\'ipa\' ph=\'mɔɪ\'>moi?</phoneme>': [
+        'soak the beans until they are soft enough to peal the skin off',
+        'use soap and water to make sure the beans are clean before cooking them',
+        'rinse the beans to get the dirt off',
+        'None of the above',
+      ],
+  },
+    {
+      'Where does <phoneme alphabet=\'ipa\' ph=\'eɡusi\'>egusi</phoneme> come from?': [
+        'melon seeds',
+        'vegetables',
+        'Soup',
+        'The Holy Spirit',
+      ],
+  },
+    {
+      'What day is World Jollof day?': [
+        'August twenty second',
+        'October first',
+        'December twenty fifth',
+        'it does not exist',
+      ],
+  },
+     {
+      'Which of the following is NOT true about palm oil?': [
+        'Palm oil is the cure for cancer',
+        'Palm oil has no trans fats',
+        'Palm oil is produced by simple steaming and pressing',
+        'Palm oil is often used as an ingredient in ice cream',
+      ],
+  },
+    {
+      'What popular form of music that originated in Ghana in the nineteen twenties was later brought to Nigeria starting in the ninteen thirties?': [
+        'High life',
+        'Afro beats',
+        'Shaku Shaku',
+        'Makossa',
+      ],
+  },
+    {
+      'Who originated the term afro beat?': [
+        'Fela Kuti',
+        'Chief Stephen <phoneme alphabet=\'ipa\' ph=\'osita\'>Osita</phoneme> <phoneme alphabet=\'ipa\' ph=\'osadebe\'>Osadebe</phoneme>',
+        'Styles Plus',
+        'Two face <phoneme alphabet=\'ipa\' ph=\'idibia\'>Idibia</phoneme>',
+      ],
+  },
+    {
+      'According to naija dot N G, in 2018 who was the richest Nigerian musician?': [
+        'Davido',
+        'Wiz kid',
+        'Tiwa Savage',
+        'Don Jazzy',
+      ],
+  },
+    {
+      'The founder of Mavin Records goes by Don Jazzy, but his real name is what?': [
+        'Michael Collins <phoneme alphabet=\'ipa\' ph=\'adʒeɹe\'>Ajereh</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'tʃibʊ̙zɔ̙ɹ\'>Chibuzor</phoneme> Nelson <phoneme alphabet=\'ipa\' ph=\'azʊ̙bike\'>Azubuike</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'oluwatobi\'>Oluwatobi</phoneme> <phoneme alphabet=\'ipa\' ph=\'wande\'>Wande</phoneme> <phoneme alphabet=\'ipa\' ph=\'odʒosipe\'>Ojosipe</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'ajodedʒi\'>Ayodeji</phoneme> <phoneme alphabet=\'ipa\' ph=\'ibɹaɦim\'>Ibrahim</phoneme> <phoneme alphabet=\'ipa\' ph=\'baloɡun\'>Balogun</phoneme>',
+      ],
+  },
+    {
+      'Which of the following are not lyrics to a Flavor song?': [
+        'Make we rest small small',
+        'Baby this your booty na confirm o',
+        'Looking for sexy sexy sexy',
+        '<phoneme alphabet=\'ipa\' ph=\'ɔ̙dɪ̙kʷa\'>Odikwa</phoneme> very risky o',
+      ],
+  },
+     {
+      'The kola nut is very sacred in <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> land. Which of the following is not true about kola nut in <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> land?': [
+        'Kola nut is buried before every ceremony as a sacrifice to the spirits',
+        '<phoneme alphabet=\'ipa\' ph=\'iboʊs\'>Igbos</phoneme> say, when the kola nut reaches home, it will tell where it came from',
+        '<phoneme alphabet=\'ipa\' ph=\'iboʊs\'>Igbos</phoneme> say, the kola nut does not understand foreign languages',
+        'In kola nut ceremonies, <phoneme alphabet=\'ipa\' ph=\'iboʊs\'>Igbos</phoneme> call their ancestors to receive the kola nut before offering it to the elders in their midst',
+      ],
+  },
+    {
+      'Other than <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> ceremonies, where is kola nut used?': [
+        'All of the above',
+        'Diet pills',
+        'Coca cola',
+        'Medicine',
+      ],
+  },
+    {
+      'Which of these has never been an implied health benefit of kola nut?': [
+        'Cure for insomnia',
+        'Aid to digestion',
+        'Increased circulation',
+        'Decreased risk of prostate cancer',
+      ],
+    },
+    {
+      'In pre-colonial <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> land, how many days were in a market week?': [
+        'four',
+        'seven',
+        'ten',
+        'twelve',
+      ],
+  },
+    {
+      'Which of the following is true about <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> wrappah?': [
+        'It is worn by both <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> Igbo </phoneme> men and <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> Igbo </phoneme> women',
+        'It is a borrowed fashion from Greek togas',
+        'It is not allowed to touch the ground',
+        'It should not be washed more than once per month',
+      ],
+  },
+    {
+      'In the Nigerian civil war, there was an attempt to separate <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> land from the rest of Nigeria. What was the new <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> country called?': [
+        'Biafra',
+        '<phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> land',
+        '<phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> world',
+        'United States of <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme>',
+        ],
+  },
+    {
+      'The first car manufacturing company in West Africa was founded by an <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> man. What is his name?': [
+        ' Innocent <phoneme alphabet=\'ipa\' ph=\'ˈtʃukʷuma\'> Chukwuma</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'iɦuɔ̙ma\'> Ihuoma</phoneme> <phoneme alphabet=\'ipa\' ph=\'ˈemenike\'>Emenike</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'ˈtʃukʷuemeka\'> Chukwuemeka</phoneme> <phoneme alphabet=\'ipa\' ph=\'ˈodʒukʷu\'>Ojukwu</phoneme>',
+        '<phoneme alphabet=\'ipa\' ph=\'ˈokonkʷɔ̙\'> Okonkwo</phoneme> of <phoneme alphabet=\'ipa\' ph=\'umɔ̙fɪ̙a\'>Umuofia</phoneme>',
+      ],
+  },
+    {
+      'Which of these places can you not find an <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> person?': [
+        'Mars, because <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> are everywhere but no man has yet traveled to Mars',
+        'South Eastern Nigeria',
+        'Northern Nigeria',
+        'Cameroon',
+      ],
+  },
+    {
+      'Which of these is not true about plam wine ceremony?': [
+        'You are not allowed to drink palm wine from calabash cups',
+        'The youngest man serves the palm wine',
+        'Titled men drink palm wine from elephant tusks or cow horns',
+        'The first drink goes to the host, the second to the youngest man who is serving before then serving the elders',
+      ],
+  },
+     {
+      'Which of these is not a traditional <phoneme alphabet=\'ipa\' ph=\'iboʊ\'> Igbo </phoneme> custom before a wedding?': [
+        'Bachelor party',
+        'Bride price payment or dowry',
+        'Wine carrying ceremony',
+        'Consent from extended family',
+      ],
+  },
+  ],
+
+};
